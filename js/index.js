1859927350b3124aa4c515c320f08431d9fcbf20
kerrwp <Wesley@microspec.com>
2013-09-06 14:34:23 -0400

    Modifed index to actually work

---
+++ js/index.js
@@ -0,0 +1,320 @@
+
+// QRCODE reader Copyright 2011 Lazar Laszlo
+// http://www.webqr.com
+
+var gCtx = null;
+var gCanvas = null;
+var imageData = null;
+var c=0;
+var stype=0;
+var gUM=false;
+var webkit=false;
+var moz=false;
+var v=null;
+
+var camhtml='  	<object  id="iembedflash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="320" height="240"> '+
+  		'<param name="movie" value="camcanvas.swf" />'+
+  		'<param name="quality" value="high" />'+
+		'<param name="allowScriptAccess" value="always" />'+
+  		'<embed  allowScriptAccess="always"  id="embedflash" src="camcanvas.swf" quality="high" width="320" height="240" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" mayscript="true"  />'+
+    '</object>';
+var imghtml='<div id="qrfile"><canvas id="out-canvas" width="320" height="240"></canvas>'+
+    '<div id="imghelp">drag and drop a QRCode here'+
+	'<br>or select a file'+
+	'<input type="file" onchange="handleFiles(this.files)"/>'+
+	'</div>'+
+'</div>';
+
+var vidhtml = '<video id="v" autoplay></video>';
+
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+}
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  var dt = e.dataTransfer;
+  var files = dt.files;
+  if(files.length>0)
+  {
+	handleFiles(files);
+  }
+  else
+  if(dt.getData('URL'))
+  {
+	qrcode.decode(dt.getData('URL'));
+  }
+}
+
+function handleFiles(f)
+{
+	var o=[];
+	
+	for(var i =0;i<f.length;i++)
+	{
+        var reader = new FileReader();
+        reader.onload = (function(theFile) {
+        return function(e) {
+            gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+
+			qrcode.decode(e.target.result);
+        };
+        })(f[i]);
+        reader.readAsDataURL(f[i]);	
+    }
+}
+
+function initCanvas(ww,hh)
+{
+    gCanvas = document.getElementById("qr-canvas");
+    var w = ww;
+    var h = hh;
+    gCanvas.style.width = w + "px";
+    gCanvas.style.height = h + "px";
+    gCanvas.width = w;
+    gCanvas.height = h;
+    gCtx = gCanvas.getContext("2d");
+    gCtx.clearRect(0, 0, w, h);
+    imageData = gCtx.getImageData( 0,0,320,240);
+}
+
+function passLine(stringPixels) { 
+
+    var coll = stringPixels.split("-");
+
+    for(var i=0;i<320;i++) { 
+        var intVal = parseInt(coll[i]);
+        r = (intVal >> 16) & 0xff;
+        g = (intVal >> 8) & 0xff;
+        b = (intVal ) & 0xff;
+        imageData.data[c+0]=r;
+        imageData.data[c+1]=g;
+        imageData.data[c+2]=b;
+        imageData.data[c+3]=255;
+        c+=4;
+    } 
+
+    if(c>=320*240*4) { 
+        c=0;
+        gCtx.putImageData(imageData, 0,0);
+        try{
+            qrcode.decode();
+        }
+        catch(e){       
+            console.log(e);
+            setTimeout(captureToCanvas, 500);
+        };
+    } 
+} 
+var count = 0;
+function captureToCanvas() {
+console.log("captureToCanvas function call"+ (++count));
+    if(stype!=1)
+        return;
+    if(gUM)
+    {
+        try{
+            gCtx.drawImage(v,0,0);
+            try{
+                qrcode.decode();
+            }
+            catch(e){       
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+            };
+        }
+        catch(e){       
+                console.log(e);
+                setTimeout(captureToCanvas, 500);
+        };
+    }
+    else
+    {
+        flash = document.getElementById("embedflash");
+        try{
+            flash.ccCapture();
+        }
+        catch(e)
+        {
+            console.log(e);
+            setTimeout(captureToCanvas, 1000);
+        }
+    }
+}
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function read(a)
+{
+    var html="<br>";
+    if(a.indexOf("http://") === 0 || a.indexOf("https://") === 0)
+        html+="<a target='_blank' href='"+a+"'>"+a+"</a><br>";
+    html+="<b>"+htmlEntities(a)+"</b><br><br>";
+    document.getElementById("result").innerHTML=html;
+}	
+
+function isCanvasSupported(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+}
+function success(stream) {
+    if(webkit)
+        v.src = window.webkitURL.createObjectURL(stream);
+    else
+    if(moz)
+    {
+        v.mozSrcObject = stream;
+        v.play();
+    }
+    else
+        v.src = stream;
+    gUM=true;
+    setTimeout(captureToCanvas, 500);
+}
+		
+function error(error) {
+    gUM=false;
+    return;
+}
+
+function load()
+{
+	if(isCanvasSupported() && window.File && window.FileReader)
+	{
+		initCanvas(800,600);
+		qrcode.callback = read;
+	//	document.getElementById("mainbody").style.display="inline";
+	}
+	else
+	{
+		document.getElementById("mainbody").style.display="inline";
+		document.getElementById("mainbody").innerHTML='<p id="mp1">QR code scanner for HTML5 capable browsers</p><br>'+
+        '<br><p id="mp2">sorry your browser is not supported</p><br><br>'+
+        '<p id="mp1">try <a href="http://www.mozilla.com/firefox"><img src="firefox.png"/></a> or <a href="http://chrome.google.com"><img src="chrome_logo.gif"/></a> or <a href="http://www.opera.com"><img src="Opera-logo.png"/></a></p>';
+	}
+}
+
+function setwebcam()
+{
+	document.getElementById("result").innerHTML="- scanning -";
+    if(stype==1)
+    {
+        setTimeout(captureToCanvas, 500);    
+        return;
+    }
+    var n=navigator;
+    if(n.getUserMedia)
+    {
+        document.getElementById("outdiv").innerHTML = vidhtml;
+        v=document.getElementById("v");
+        n.getUserMedia({video: true, audio: false}, success, error);
+    }
+    else
+    if(n.webkitGetUserMedia)
+    {
+        document.getElementById("outdiv").innerHTML = vidhtml;
+        v=document.getElementById("v");
+        webkit=true;
+        n.webkitGetUserMedia({video: true, audio: false}, success, error);
+    }
+    else
+    if(n.mozGetUserMedia)
+    {
+        document.getElementById("outdiv").innerHTML = vidhtml;
+        v=document.getElementById("v");
+        moz=true;
+        n.mozGetUserMedia({video: true, audio: false}, success, error);
+        
+    }
+    else
+        document.getElementById("outdiv").innerHTML = camhtml;
+    stype=1;
+    setTimeout(captureToCanvas, 500);
+}
+
+function setimg()
+{
+	document.getElementById("result").innerHTML="";
+    if(stype==2)
+        return;
+    document.getElementById("outdiv").innerHTML = imghtml;
+    var qrfile = document.getElementById("qrfile");
+    qrfile.addEventListener("dragenter", dragenter, false);  
+    qrfile.addEventListener("dragover", dragover, false);  
+    qrfile.addEventListener("drop", drop, false);
+    stype=2;
+}
+
+
+ cordova.exec(function(winParam) {}, function(error) {}, "service",
+                 "action", ["firstArgument", "secondArgument", 42,
+                 false]);
+
+ var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
+
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready",onDeviceReady,false);
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+// A button will call this function
+    //
+function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
+		
+		
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+app.initialize();
